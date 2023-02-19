885ce82459fb82fb2636fb31f6b4f4c4c11ec02b
AnkuNoName <ankunoname@gmail.com>
2023-02-19 12:42:36 +0200

    react-project v1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header className='header'>
+        <div className='header__inner'>
+          <div className='header__inner-logo'>
+            <img className="header-logo" alt="" src="/img/header-logo.png" />
+            <div className="header__logo-text">
+              <h3>
+                REACT SNEAKERS
+              </h3>
+              <p>
+                Магазин лучших кроссовок
+              </p>
+            </div>
+          </div>
+          <div className='header__inner-right'>
+            <ul className="header__inner-list">
+              <li>
+                <img className="header-logo" alt="" src="/img/cart.svg" />
+                <span>1205 грн.</span>
+              </li>
+              <li>
+                <img className="header-logo" alt="" src="/img/user.svg" />
+              </li>
+            </ul>
+          </div>
+        </div>    
       </header>
+      <main className="main">
+        <container className="container">
+          <div className="main__inner">
+            <div className="main__inner-slider">
+              <img className="main__inner-logo" alt="" src="/img/main-slider-1.jpg" />
+            </div>
+
+            <div className="main__inner-content">
+              <h3 className="main__inner-title">
+                Все кроссовки
+              </h3>
+            </div>
+
+          </div>
+        </container>
+      </main>      
     </div>
   );
 }
